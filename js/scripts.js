a99844e0b36ca2d400075f3cc81e22a899cdbe90
carol-wambui <carolwambuidaystar@gmail.com>
2020-10-11 23:38:36 +0300

    style footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
-$(".design").click(function(){
+$(".design").click(function () {
     $(".design").hide();
     $(".design-p").show();
 })
-$(".design-p").click(function(){
+$(".design-p").click(function () {
     $(".design-p").hide();
     $(".design").show();
 })
-$(".development").click(function(){
+$(".development").click(function () {
     $(".development").hide();
     $(".development-p").show();
 })
-$(".development-p").click(function(){
+$(".development-p").click(function () {
     $(".development-p").hide();
     $(".development").show();
 })
-$(".product").click(function(){
+$(".product").click(function () {
     $(".product").hide();
     $(".product-p").show();
 })
- $(".product-p").click(function(){
- $(".product-p").hide();
- $(".product").show();
+$(".product-p").click(function () {
+    $(".product-p").hide();
+    $(".product").show();
 })
